610ed37dbecc4c40f578c256ea93d60fa378ba01
Jinvirle <jinvirle@gmail.com>
2018-12-13 16:54:10 +0800

    Add open graph meta tag on home page

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -10,8 +10,24 @@ use JiNexus\Mvc\Model\ViewModel;
  */
 class IndexController extends AbstractController
 {
+    /**
+     * @return ViewModel
+     * @throws \JiNexus\Route\Exception
+     */
     public function indexAction()
     {
-        return new ViewModel();
+        $request = $this->http->getRequest();
+
+        return new ViewModel([
+            'title' => 'JiNexus Framework',
+            'meta' => [
+                'description' => 'A modular, lightweight and easy to use PHP framework and probably the smallest and fastest MVC framework',
+                'og' => [
+                    'url' => $request->baseUrl() . $this->view->url('application.home'),
+                    'description' => 'A modular, lightweight and easy to use PHP framework and probably the smallest and fastest MVC framework',
+                    'image' => $request->baseUrl() . $this->view->basePath('asset/img/cover-photo/jinexus-framework-home-cover.png'),
+                ],
+            ],
+        ]);
     }
 }
